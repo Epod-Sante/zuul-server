18a634f8d64867a5f3f89f970972c3cc09fcf1a5
Lahcene Zinnour <lahcenezinnour@gmail.com>
2019-11-11 01:33:46 +0000

    JWT: RSA encrypt

--- src/main/java/ca/uqtr/zuulserver/security/GatewayConfiguration.java
+++ src/main/java/ca/uqtr/zuulserver/security/GatewayConfiguration.java
@@ -1,9 +1,12 @@
 package ca.uqtr.zuulserver.security;
 
+import io.micrometer.core.instrument.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -14,6 +17,8 @@ import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.IOException;
 
 @Configuration
 @EnableResourceServer
@@ -31,18 +36,49 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/**")
                 .authenticated();
-
-
     }
 
-    @Primary
+    /*@Primary
     @Bean
     public RemoteTokenServices tokenService() {
         RemoteTokenServices tokenService = new RemoteTokenServices();
         tokenService.setCheckTokenEndpointUrl(
                 AuthorizationServerBaseURL+"/api/v1/auth-server/oauth/check_token");
-        tokenService.setClientId("clientId");
+        tokenService.setClientId("SPA");
         tokenService.setClientSecret("secret");
         return tokenService;
+    }*/
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer config) {
+        config.tokenServices(tokenServices());
+    }
+
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        Resource resource = new ClassPathResource("public.txt");
+        String publicKey = null;
+        try {
+            publicKey = IOUtils.toString(resource.getInputStream());
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+        converter.setVerifierKey(publicKey);
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
     }
 }
