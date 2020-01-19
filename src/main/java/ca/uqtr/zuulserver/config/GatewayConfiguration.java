172f2cb3ba46e7e1fa1f7f07cd1ea32e6162aae0
zinnourlahcene <zinnour@uqtr.ca>
2020-01-19 00:35:23 +0000

    heroku 1

--- src/main/java/ca/uqtr/zuulserver/config/GatewayConfiguration.java
+++ src/main/java/ca/uqtr/zuulserver/config/GatewayConfiguration.java
@@ -40,11 +40,12 @@ public class GatewayConfiguration extends ResourceServerConfigurerAdapter {
        http.authorizeRequests()
                 .antMatchers("/api/v1/auth-server/oauth/**",
                         "/api/v1/auth-server/registration",
-                        "/api/v1/auth-server/docs")
+                        "/api/v1/auth-server/docs",
+                        "/api/v1/patient/all")
                 .permitAll()
                 .antMatchers("/**")
                 .authenticated();
-        
+
     }
 
     @Primary
