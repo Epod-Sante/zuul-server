7ed427999bfa669ee9367aaf35ce9fc07ba6c21c
zinnourlahcene <zinnour@uqtr.ca>
2020-03-20 20:02:04 -0400

    token validity

--- src/main/java/ca/uqtr/zuulserver/config/RedisConfiguration.java
+++ src/main/java/ca/uqtr/zuulserver/config/RedisConfiguration.java
@@ -1,42 +1,29 @@
 package ca.uqtr.zuulserver.config;
 
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
 public class RedisConfiguration {
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory() throws URISyntaxException {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        RedisProperties properties = redisProperties();
-        URI redisURI = new URI(System.getenv(properties.getUrl()));
-        configuration.setHostName(redisURI.getHost());
-        configuration.setPort(redisURI.getPort());
-
-        return new JedisConnectionFactory(configuration);
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName("ec2-34-203-3-176.compute-1.amazonaws.com");
+        jedisConnectionFactory.setPort(21369);
+        jedisConnectionFactory.setPassword("pda31645dbaf43d93b4aecb0dee8c7cd26b2b94056e2b4ce9a2960e0508c49aea");
+        return jedisConnectionFactory;
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() throws URISyntaxException {
+    public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
         return template;
-    }
-    @Bean
-    @Primary
-    public RedisProperties redisProperties() {
-        return new RedisProperties();
     }
 
 }
